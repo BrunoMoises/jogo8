841d322c5521d617d736737e999cbe242f9538c9
Bruno <bruno_moises_stz@hotmail.com>
2022-09-12 23:32:24 -0300

    Inserindo valores nos botões para o jogo ser iniciado, e elaborando a logica para cada botão ao ser clicado

--- src/main/java/br/com/bmoises/jogo8/No.java
+++ src/main/java/br/com/bmoises/jogo8/No.java
@@ -1,17 +1,11 @@
 package br.com.bmoises.jogo8;
 
-import java.util.ArrayList;
-
 public class No {
 
     int[][] estado;
-    ArrayList<No> filho = new ArrayList();
-    No pai;
 
-    public No(int[][] estado) {
-        this.estado = estado;
-        filho = null;
-        pai = null;
+    public No() {
+        this.estado = new int[3][3];
     }
 
     public int[][] getEstado() {
@@ -20,27 +14,6 @@ public class No {
 
     public void setEstado(int[][] estado) {
         this.estado = estado;
-    }
-
-    public ArrayList<No> getFilho() {
-        return filho;
-    }
-
-    public void setFilho(ArrayList<No> filho) {
-        this.filho = filho;
-        if(filho != null) {
-            for(No f:filho) {
-                f.pai = this;
-            }
-        }
-    }
-
-    public No getPai() {
-        return pai;
-    }
-
-    public void setPai(No pai) {
-        this.pai = pai;
     }
 
 }
--- src/main/java/br/com/bmoises/jogo8/TelaJogo.java
+++ src/main/java/br/com/bmoises/jogo8/TelaJogo.java
@@ -4,14 +4,19 @@
  */
 package br.com.bmoises.jogo8;
 
+import java.awt.Color;
+import java.util.Arrays;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author bruno
  */
-public class TelaJogo extends javax.swing.JFrame {
+public final class TelaJogo extends javax.swing.JFrame {
 
     int[][] inicio;
-    static int[][] solucao = {{1, 2, 3}, {4, 5, 6}, {7, 8, 0}};
+    No estado = new No();
+    int[][] solucao;
 
     /**
      * Creates new form TelaJogo
@@ -20,7 +25,188 @@ public class TelaJogo extends javax.swing.JFrame {
         initComponents();
         this.setTitle("Jogo dos 8");
         inicio = new int[3][3];
+        solucao = new int[3][3];
         gerarJogo();
+    }
+
+    public void gerarJogo() {
+        
+        solucao[0][0] = 1;
+        solucao[0][1] = 2;
+        solucao[0][2] = 3;
+        solucao[1][0] = 4;
+        solucao[1][1] = 5;
+        solucao[1][2] = 6;
+        solucao[2][0] = 7;
+        solucao[2][1] = 8;
+        solucao[2][2] = 0;
+        
+        bt1.setBackground(Color.getHSBColor(240, 240, 240));
+        bt2.setBackground(Color.getHSBColor(240, 240, 240));
+        bt3.setBackground(Color.getHSBColor(240, 240, 240));
+        bt4.setBackground(Color.getHSBColor(240, 240, 240));
+        bt5.setBackground(Color.getHSBColor(240, 240, 240));
+        bt6.setBackground(Color.getHSBColor(240, 240, 240));
+        bt7.setBackground(Color.getHSBColor(240, 240, 240));
+        bt8.setBackground(Color.getHSBColor(240, 240, 240));
+        bt9.setBackground(Color.getHSBColor(240, 240, 240));
+
+        int numero = (int) (Math.random() * 6) + 1;
+
+        switch (numero) {
+
+            case 1 -> {
+                inicio[0][0] = 1;
+                inicio[0][1] = 2;
+                inicio[0][2] = 3;
+                inicio[1][0] = 4;
+                inicio[1][1] = 0;
+                inicio[1][2] = 6;
+                inicio[2][0] = 7;
+                inicio[2][1] = 5;
+                inicio[2][2] = 8;
+
+                bt1.setText("1");
+                bt2.setText("2");
+                bt3.setText("3");
+                bt4.setText("4");
+                bt5.setText("0");
+                bt5.setBackground(Color.red);
+                bt6.setText("6");
+                bt7.setText("7");
+                bt8.setText("5");
+                bt9.setText("8");
+            }
+
+            case 2 -> {
+                inicio[0][0] = 1;
+                inicio[0][1] = 0;
+                inicio[0][2] = 3;
+                inicio[1][0] = 4;
+                inicio[1][1] = 2;
+                inicio[1][2] = 6;
+                inicio[2][0] = 7;
+                inicio[2][1] = 5;
+                inicio[2][2] = 8;
+
+                bt1.setText("1");
+                bt2.setText("0");
+                bt2.setBackground(Color.red);
+                bt3.setText("3");
+                bt4.setText("4");
+                bt5.setText("2");
+                bt6.setText("6");
+                bt7.setText("7");
+                bt8.setText("5");
+                bt9.setText("8");
+            }
+
+            case 3 -> {
+                inicio[0][0] = 4;
+                inicio[0][1] = 1;
+                inicio[0][2] = 3;
+                inicio[1][0] = 0;
+                inicio[1][1] = 2;
+                inicio[1][2] = 6;
+                inicio[2][0] = 7;
+                inicio[2][1] = 5;
+                inicio[2][2] = 8;
+
+                bt1.setText("4");
+                bt2.setText("1");
+                bt3.setText("3");
+                bt4.setText("0");
+                bt4.setBackground(Color.red);
+                bt5.setText("2");
+                bt6.setText("6");
+                bt7.setText("7");
+                bt8.setText("5");
+                bt9.setText("8");
+            }
+
+            case 4 -> {
+                inicio[0][0] = 4;
+                inicio[0][1] = 1;
+                inicio[0][2] = 3;
+                inicio[1][0] = 2;
+                inicio[1][1] = 6;
+                inicio[1][2] = 8;
+                inicio[2][0] = 7;
+                inicio[2][1] = 5;
+                inicio[2][2] = 0;
+
+                bt1.setText("4");
+                bt2.setText("1");
+                bt3.setText("3");
+                bt4.setText("2");
+                bt5.setText("6");
+                bt6.setText("8");
+                bt7.setText("7");
+                bt8.setText("5");
+                bt9.setText("0");
+                bt9.setBackground(Color.red);
+            }
+
+            case 5 -> {
+                inicio[0][0] = 4;
+                inicio[0][1] = 1;
+                inicio[0][2] = 3;
+                inicio[1][0] = 2;
+                inicio[1][1] = 6;
+                inicio[1][2] = 8;
+                inicio[2][0] = 7;
+                inicio[2][1] = 5;
+                inicio[2][2] = 0;
+
+                bt1.setText("4");
+                bt2.setText("1");
+                bt3.setText("3");
+                bt4.setText("2");
+                bt5.setText("6");
+                bt6.setText("8");
+                bt7.setText("7");
+                bt8.setText("5");
+                bt9.setText("0");
+                bt9.setBackground(Color.red);
+            }
+
+            case 6 -> {
+                inicio[0][0] = 6;
+                inicio[0][1] = 0;
+                inicio[0][2] = 1;
+                inicio[1][0] = 2;
+                inicio[1][1] = 7;
+                inicio[1][2] = 3;
+                inicio[2][0] = 4;
+                inicio[2][1] = 5;
+                inicio[2][2] = 8;
+
+                bt1.setText("6");
+                bt2.setText("0");
+                bt2.setBackground(Color.red);
+                bt3.setText("1");
+                bt4.setText("2");
+                bt5.setText("7");
+                bt6.setText("3");
+                bt7.setText("4");
+                bt8.setText("5");
+                bt9.setText("8");
+            }
+
+        }
+
+        estado.setEstado(inicio);
+    }
+
+    public void ganhou() {
+
+        inicio = estado.getEstado();
+        
+        if (Arrays.equals(inicio, solucao)) {
+            JOptionPane.showMessageDialog(null, "Parabéns você resolveu o Puzzle!");
+            gerarJogo();
+        }
+
     }
 
     /**
@@ -155,39 +341,362 @@ public class TelaJogo extends javax.swing.JFrame {
     }//GEN-LAST:event_bt1ActionPerformed
 
     private void bt1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_bt1MouseClicked
-        // TODO add your handling code here:
+        inicio = estado.getEstado();
+        if ("0".equals(bt4.getText())) {
+            int btn4 = inicio[1][0];
+            int btn1 = inicio[0][0];
+
+            inicio[0][0] = btn4;
+            inicio[1][0] = btn1;
+
+            bt4.setText(Integer.toString(btn1));
+            bt1.setText(Integer.toString(btn4));
+            bt1.setBackground(Color.red);
+            bt4.setBackground(Color.getHSBColor(240, 240, 240));
+        }
+
+        if ("0".equals(bt2.getText())) {
+            int btn2 = inicio[0][1];
+            int btn1 = inicio[0][0];
+
+            inicio[0][0] = btn2;
+            inicio[0][1] = btn1;
+
+            bt2.setText(Integer.toString(btn1));
+            bt1.setText(Integer.toString(btn2));
+            bt1.setBackground(Color.red);
+            bt2.setBackground(Color.getHSBColor(240, 240, 240));
+        }
+        estado.setEstado(inicio);
+
+        ganhou();
     }//GEN-LAST:event_bt1MouseClicked
 
     private void bt2MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_bt2MouseClicked
-        // TODO add your handling code here:
+        inicio = estado.getEstado();
+        if ("0".equals(bt5.getText())) {
+            int btn5 = inicio[1][1];
+            int btn2 = inicio[0][1];
+
+            inicio[0][1] = btn5;
+            inicio[1][1] = btn2;
+
+            bt5.setText(Integer.toString(btn2));
+            bt2.setText(Integer.toString(btn5));
+            bt2.setBackground(Color.red);
+            bt5.setBackground(Color.getHSBColor(240, 240, 240));
+        }
+
+        if ("0".equals(bt3.getText())) {
+            int btn3 = inicio[0][2];
+            int btn2 = inicio[0][1];
+
+            inicio[0][1] = btn3;
+            inicio[0][2] = btn2;
+
+            bt3.setText(Integer.toString(btn2));
+            bt2.setText(Integer.toString(btn3));
+            bt2.setBackground(Color.red);
+            bt3.setBackground(Color.getHSBColor(240, 240, 240));
+        }
+
+        if ("0".equals(bt1.getText())) {
+            int btn1 = inicio[0][0];
+            int btn2 = inicio[0][1];
+
+            inicio[0][0] = btn2;
+            inicio[0][1] = btn1;
+
+            bt1.setText(Integer.toString(btn2));
+            bt2.setText(Integer.toString(btn1));
+            bt2.setBackground(Color.red);
+            bt1.setBackground(Color.getHSBColor(240, 240, 240));
+        }
+        estado.setEstado(inicio);
+
+        ganhou();
     }//GEN-LAST:event_bt2MouseClicked
 
     private void bt3MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_bt3MouseClicked
-        // TODO add your handling code here:
+        inicio = estado.getEstado();
+        if ("0".equals(bt6.getText())) {
+            int btn3 = inicio[0][2];
+            int btn6 = inicio[1][2];
+
+            inicio[0][2] = btn6;
+            inicio[1][2] = btn3;
+
+            bt6.setText(Integer.toString(btn3));
+            bt3.setText(Integer.toString(btn6));
+            bt3.setBackground(Color.red);
+            bt6.setBackground(Color.getHSBColor(240, 240, 240));
+        }
+
+        if ("0".equals(bt2.getText())) {
+            int btn3 = inicio[0][2];
+            int btn2 = inicio[0][1];
+
+            inicio[0][1] = btn3;
+            inicio[0][2] = btn2;
+
+            bt2.setText(Integer.toString(btn3));
+            bt3.setText(Integer.toString(btn2));
+            bt3.setBackground(Color.red);
+            bt2.setBackground(Color.getHSBColor(240, 240, 240));
+        }
     }//GEN-LAST:event_bt3MouseClicked
 
     private void bt4MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_bt4MouseClicked
-        // TODO add your handling code here:
+        inicio = estado.getEstado();
+        if ("0".equals(bt7.getText())) {
+            int btn4 = inicio[1][0];
+            int btn7 = inicio[2][0];
+
+            inicio[2][0] = btn4;
+            inicio[1][0] = btn7;
+
+            bt7.setText(Integer.toString(btn4));
+            bt4.setText(Integer.toString(btn7));
+            bt4.setBackground(Color.red);
+            bt7.setBackground(Color.getHSBColor(240, 240, 240));
+        }
+
+        if ("0".equals(bt1.getText())) {
+            int btn4 = inicio[1][0];
+            int btn1 = inicio[0][0];
+
+            inicio[1][0] = btn1;
+            inicio[0][0] = btn4;
+
+            bt1.setText(Integer.toString(btn4));
+            bt4.setText(Integer.toString(btn1));
+            bt4.setBackground(Color.red);
+            bt1.setBackground(Color.getHSBColor(240, 240, 240));
+        }
+
+        if ("0".equals(bt5.getText())) {
+            int btn4 = inicio[1][0];
+            int btn5 = inicio[1][1];
+
+            inicio[1][0] = btn5;
+            inicio[1][1] = btn4;
+
+            bt5.setText(Integer.toString(btn4));
+            bt4.setText(Integer.toString(btn5));
+            bt4.setBackground(Color.red);
+            bt5.setBackground(Color.getHSBColor(240, 240, 240));
+        }
+        estado.setEstado(inicio);
+
+        ganhou();
     }//GEN-LAST:event_bt4MouseClicked
 
     private void bt5MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_bt5MouseClicked
-        // TODO add your handling code here:
+        inicio = estado.getEstado();
+        if ("0".equals(bt8.getText())) {
+            int btn5 = inicio[1][1];
+            int btn8 = inicio[2][1];
+
+            inicio[2][1] = btn5;
+            inicio[1][1] = btn8;
+
+            bt8.setText(Integer.toString(btn5));
+            bt5.setText(Integer.toString(btn8));
+            bt5.setBackground(Color.red);
+            bt8.setBackground(Color.getHSBColor(240, 240, 240));
+        }
+
+        if ("0".equals(bt4.getText())) {
+            int btn5 = inicio[1][1];
+            int btn4 = inicio[1][0];
+
+            inicio[1][0] = btn5;
+            inicio[1][1] = btn4;
+
+            bt4.setText(Integer.toString(btn5));
+            bt5.setText(Integer.toString(btn4));
+            bt5.setBackground(Color.red);
+            bt4.setBackground(Color.getHSBColor(240, 240, 240));
+        }
+
+        if ("0".equals(bt2.getText())) {
+            int btn5 = inicio[1][1];
+            int btn2 = inicio[0][1];
+
+            inicio[0][1] = btn5;
+            inicio[1][1] = btn2;
+
+            bt2.setText(Integer.toString(btn5));
+            bt5.setText(Integer.toString(btn2));
+            bt5.setBackground(Color.red);
+            bt2.setBackground(Color.getHSBColor(240, 240, 240));
+        }
+
+        if ("0".equals(bt6.getText())) {
+            int btn5 = inicio[1][1];
+            int btn6 = inicio[1][2];
+
+            inicio[1][1] = btn6;
+            inicio[1][2] = btn5;
+
+            bt6.setText(Integer.toString(btn5));
+            bt5.setText(Integer.toString(btn6));
+            bt5.setBackground(Color.red);
+            bt6.setBackground(Color.getHSBColor(240, 240, 240));
+        }
+        estado.setEstado(inicio);
+
+        ganhou();
     }//GEN-LAST:event_bt5MouseClicked
 
     private void bt6MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_bt6MouseClicked
-        // TODO add your handling code here:
+        inicio = estado.getEstado();
+        if ("0".equals(bt9.getText())) {
+            int btn6 = inicio[1][2];
+            int btn9 = inicio[2][2];
+
+            inicio[2][2] = btn6;
+            inicio[1][2] = btn9;
+
+            bt9.setText(Integer.toString(btn6));
+            bt6.setText(Integer.toString(btn9));
+            bt6.setBackground(Color.red);
+            bt9.setBackground(Color.getHSBColor(240, 240, 240));
+        }
+
+        if ("0".equals(bt5.getText())) {
+            int btn6 = inicio[1][2];
+            int btn5 = inicio[1][1];
+
+            inicio[1][1] = btn6;
+            inicio[1][2] = btn5;
+
+            bt5.setText(Integer.toString(btn6));
+            bt6.setText(Integer.toString(btn5));
+            bt6.setBackground(Color.red);
+            bt5.setBackground(Color.getHSBColor(240, 240, 240));
+        }
+
+        if ("0".equals(bt3.getText())) {
+            int btn6 = inicio[1][2];
+            int btn3 = inicio[0][2];
+
+            inicio[0][2] = btn6;
+            inicio[1][2] = btn3;
+
+            bt3.setText(Integer.toString(btn6));
+            bt6.setText(Integer.toString(btn3));
+            bt6.setBackground(Color.red);
+            bt3.setBackground(Color.getHSBColor(240, 240, 240));
+        }
+        estado.setEstado(inicio);
+
+        ganhou();
     }//GEN-LAST:event_bt6MouseClicked
 
     private void bt7MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_bt7MouseClicked
-        // TODO add your handling code here:
+        inicio = estado.getEstado();
+        if ("0".equals(bt8.getText())) {
+            int btn8 = inicio[2][1];
+            int btn7 = inicio[2][0];
+
+            inicio[2][0] = btn8;
+            inicio[2][1] = btn7;
+
+            bt8.setText(Integer.toString(btn7));
+            bt7.setText(Integer.toString(btn8));
+            bt7.setBackground(Color.red);
+            bt8.setBackground(Color.getHSBColor(240, 240, 240));
+        }
+
+        if ("0".equals(bt4.getText())) {
+            int btn4 = inicio[1][0];
+            int btn7 = inicio[2][0];
+
+            inicio[2][0] = btn4;
+            inicio[1][0] = btn7;
+
+            bt4.setText(Integer.toString(btn7));
+            bt7.setText(Integer.toString(btn4));
+            bt7.setBackground(Color.red);
+            bt4.setBackground(Color.getHSBColor(240, 240, 240));
+        }
     }//GEN-LAST:event_bt7MouseClicked
 
     private void bt8MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_bt8MouseClicked
-        // TODO add your handling code here:
+        inicio = estado.getEstado();
+        if ("0".equals(bt9.getText())) {
+            int btn8 = inicio[2][1];
+            int btn9 = inicio[2][2];
+
+            inicio[2][1] = btn9;
+            inicio[2][2] = btn8;
+
+            bt9.setText(Integer.toString(btn8));
+            bt8.setText(Integer.toString(btn9));
+            bt8.setBackground(Color.red);
+            bt9.setBackground(Color.getHSBColor(240, 240, 240));
+        }
+        if ("0".equals(bt5.getText())) {
+            int btn5 = inicio[1][1];
+            int btn8 = inicio[2][1];
+
+            inicio[1][1] = btn8;
+            inicio[2][1] = btn5;
+
+            bt5.setText(Integer.toString(btn8));
+            bt8.setText(Integer.toString(btn5));
+            bt8.setBackground(Color.red);
+            bt5.setBackground(Color.getHSBColor(240, 240, 240));
+        }
+
+        if ("0".equals(bt7.getText())) {
+            int btn7 = inicio[2][0];
+            int btn8 = inicio[2][1];
+
+            inicio[2][0] = btn8;
+            inicio[2][1] = btn7;
+
+            bt7.setText(Integer.toString(btn8));
+            bt8.setText(Integer.toString(btn7));
+            bt8.setBackground(Color.red);
+            bt7.setBackground(Color.getHSBColor(240, 240, 240));
+        }
+        estado.setEstado(inicio);
+
+        ganhou();
     }//GEN-LAST:event_bt8MouseClicked
 
     private void bt9MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_bt9MouseClicked
-        // TODO add your handling code here:
+        inicio = estado.getEstado();
+        if ("0".equals(bt6.getText())) {
+            int btn6 = inicio[1][2];
+            int btn9 = inicio[2][2];
+
+            inicio[2][2] = btn6;
+            inicio[1][2] = btn9;
+
+            bt6.setText(Integer.toString(btn9));
+            bt9.setText(Integer.toString(btn6));
+            bt9.setBackground(Color.red);
+            bt6.setBackground(Color.getHSBColor(240, 240, 240));
+        }
+
+        if ("0".equals(bt8.getText())) {
+            int btn8 = inicio[2][1];
+            int btn9 = inicio[2][2];
+
+            inicio[2][2] = btn8;
+            inicio[2][1] = btn9;
+
+            bt8.setText(Integer.toString(btn9));
+            bt9.setText(Integer.toString(btn8));
+            bt9.setBackground(Color.red);
+            bt8.setBackground(Color.getHSBColor(240, 240, 240));
+        }
+        estado.setEstado(inicio);
+
+        ganhou();
     }//GEN-LAST:event_bt9MouseClicked
 
     /**
@@ -218,10 +727,8 @@ public class TelaJogo extends javax.swing.JFrame {
         //</editor-fold>
 
         /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new TelaJogo().setVisible(true);
-            }
+        java.awt.EventQueue.invokeLater(() -> {
+            new TelaJogo().setVisible(true);
         });
     }
 

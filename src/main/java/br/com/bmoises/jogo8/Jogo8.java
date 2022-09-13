c371e2cc83c9696cfef4cbb788f17828e8ea46a3
Bruno <bruno_moises_stz@hotmail.com>
2022-09-12 22:11:53 -0300

    Commit inicial - Criado a Classe Nó, onde se armazena o estado atual da matriz e criado a Tela de exibição do jogo

---
+++ src/main/java/br/com/bmoises/jogo8/Jogo8.java
@@ -0,0 +1,11 @@
+package br.com.bmoises.jogo8;
+
+public class Jogo8 {
+
+    public static void main(String[] args) {
+        TelaJogo abrir = new TelaJogo();
+        abrir.setVisible(true);
+        abrir.setLocationRelativeTo(null);
+        abrir.setResizable(false);
+    }
+}
